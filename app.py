30b1c81875a1aa06997a3d3111c99ed25ba3fd8c
ImKunYoung <ur2kunyoung2@outlook.com>
2023-03-08 19:38:34 +0900

    fix: Update Partly

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main():
+    return 'welcome!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
