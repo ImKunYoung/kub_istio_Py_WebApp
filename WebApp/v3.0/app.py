4e733b4f2145659168f256da3f59aa627126f83a
imkunyoung <lim8667@gmail.com>
2023-03-20 15:06:51 +0900

    Add app.py v3

---
+++ WebApp/v3.0/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import datetime
+import time
+
+time.sleep(10)
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return 'Welcome user! current time is: ' + str(datetime.datetime.now())
+
+
+@app.route('/health')
+def health():
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
