1b85c39e9300cba73ffb2f9632177fbf03e670bd
ImKunYoung <ur2kunyoung2@outlook.com>
2023-03-22 16:26:42 +0900

    feat: Add GreetingService.java

---
+++ Ch02/eureka-client/src/main/java/com/example/eurekaclient/GreetingService.java
@@ -0,0 +1,33 @@
+package com.example.eurekaclient;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+/**
+ * @author : ImKunYoung
+ * @since : 2023-03-22
+ * description    :
+ * <p>
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-03-22        ImKunYoung       최초 생성
+ */
+@Service
+@RequiredArgsConstructor
+public class GreetingService {
+
+    private final RestTemplate restTemplate;
+
+
+    public String greet(String username) {
+        URI uri = URI.create("http://eureka-service/greeting/" + username);
+        return this.restTemplate.getForObject(uri, String.class);
+    }
+
+
+
+}
