d65f227d1cbb2ad540787598e9306d6b61358a6e
ImKunYoung <ur2kunyoung2@outlook.com>
2023-03-23 11:08:12 +0900

    chore: Update Config

---
+++ Ch02/eureka-client/src/main/java/com/example/eurekaclient/AppConfig.java
@@ -0,0 +1,25 @@
+package com.example.eurekaclient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author : ImKunYoung
+ * @since : 2023-03-23
+ * description    :
+ * <p>
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-03-23        ImKunYoung       최초 생성
+ */
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
